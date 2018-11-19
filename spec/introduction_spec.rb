9dc7a04c4b4498a158e8aa86649abcdb16e5765e
Alyssa Kennedy <alyssadrandall@gmail.com>
2018-11-19 02:06:05 +0000

    Done.

--- spec/introduction_spec.rb
+++ spec/introduction_spec.rb
@@ -1,9 +1,17 @@
 require "spec_helper"
 
+def introduction(name)
+  puts "Hi, my name is #{name}."
+end
+
 describe "#introduction" do 
   it "takes in an argument of a name and puts out a phrase with that name using string interpolation" do 
     expect{introduction("Josh")}.to output("Hi, my name is Josh.\n").to_stdout
   end
+end
+
+def introduction_with_language(name, language)
+  puts "Hi, my name is #{name} and I am learning to program in #{language}."
 end
 
 describe "#introduction_with_language" do 
